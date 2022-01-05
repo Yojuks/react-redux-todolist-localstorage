6e481dfe1475d3b35b745e3a006b70dac05d38aa
AndriiYozh <yojuks@gmail.com>
2022-01-05 15:44:45 +0200

    Add not working middleware as a future features to delete completed elements

---
+++ src/middlewares/addLocalStorage.js
@@ -0,0 +1,16 @@
+// import { loadState, saveState } from "../reducers/localStorage";
+
+export const addLocalStorage = (store) => (next) => (action) => {
+  //   const persistedState = loadState();
+  //   console.log(persistedState, "persistedState");
+  //   store.subscribe(() => {
+  //     const { tasks, visibilityFilters } = store.getState();
+  //     localStorage.setItem("tasks", tasks);
+  //     localStorage.setItem("visibilityFilters", visibilityFilters);
+  //   });
+  //   saveState({
+  //     tasks: store.getState().tasks,
+  //   });
+  //   let result = next(action);
+  //   return result;
+};
